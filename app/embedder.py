7d8f7a1627429616db007ba3bf7e8ea18600f16a
Prakash-103 <103nalliahprakash@gmail.com>
2025-07-02 12:33:55 +0530

    Delete outdated PDF documents: "Layer 3 Etherchannel on Cisco IOS Switch" and "Cisco IOS SPAN and RSPAN" from the repository.

--- app/embedder.py
+++
@@ -1,7 +0,0 @@
-from langchain.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
-
-def create_vector_db(documents):
-    embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
-    db = FAISS.from_documents(documents, embeddings)
-    return db
